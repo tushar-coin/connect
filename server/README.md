94222f937bbe75270421bc1a83d89716784a5af5
Manavjeet <manavdandiwal1111@gmail.com>
2024-06-07 22:53:24 +0530

    feat-frontend

--- server/README.md
+++
@@ -1,2 +0,0 @@
-# connect
-A website where one can make account react to post and create posts.
--- server/src/graphql/user/index.ts
+++
@@ -1,6 +0,0 @@
-import {mutations} from "./mutations"
-import {typeDefs} from "./typedef"
-import {queries} from "./queries"
-import {resolvers} from "./resolvers"
-
-export const User={typeDefs,resolvers,mutations,queries};
