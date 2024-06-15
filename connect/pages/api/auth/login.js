eac178c1ae0836722f64e3cf81ff61b452fe8fa3
Manavjeet <manavdandiwal1111@gmail.com>
2024-06-15 17:33:45 +0530

    redirect to login page when user not logged in

--- connect/pages/api/auth/login.js
+++ connect/pages/api/auth/login.js
@@ -5,74 +5,71 @@ const User = require("@/models/user.js");
 const jwt = require("jsonwebtoken");
 
 const handler = async (req, res) => {
-  const token = getToken(req);
-  if (token) {
-    res.status(200).json({ success: true, message: "Logged in" });
-    return;
-  }
-  const { method, body } = req;
-  if (method != "POST") {
-    res.status(401).json({
-      success: false,
-      message: "Method not allowed",
-    });
-    return;
-  }
-
-  try {
-    const db = await dbConnect();
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "Database connection failed",
-    });
-    return;
-  }
-  const secret = process.env.JWT_SECRET;
-  let user = body.user;
-
-  const check = await User.findOne({ email: user.email });
-  if (!check) {
-    res.status(401).json({
-      success: false,
-      message: "User Not Found",
-    });
-    return;
-  }
-
-  try {
-    const comp = await bcrypt.compareSync(user.password, check.password);
-    if (comp) {
-      const token = jwt.sign(
-        {
-          email: user.email,
-          firstName: check.firstName,
-          lastName: check.lastName,
-        },
-        secret,
-        {
-          expiresIn: 3600,
-        }
-      );
-      res.setHeader(
-        "Set-Cookie",
-        `token=${token}; Max-Age=3600; Path=/; HttpOnly`
-      );
-      res.status(200).json({ success: true, message: "Logged in" });
-    } else {
-      res.status(401).json({
-        success: false,
-        message: "User Not Found",
-      });
-      return;
+    const token = getToken(req);
+    if (token) {
+        res.status(200).json({ success: true, message: "Logged in" });
+        return;
     }
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "Server Error, try again later",
-    });
-    return;
-  }
+    const { method, body } = req;
+    if (method != "POST") {
+        res.status(401).json({
+            success: false,
+            message: "Method not allowed",
+        });
+        return;
+    }
+
+    try {
+        const db = await dbConnect();
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Database connection failed",
+        });
+        return;
+    }
+    const secret = process.env.JWT_SECRET;
+    let user = body.user;
+
+    const check = await User.findOne({ email: user.email });
+    if (!check) {
+        res.status(401).json({
+            success: false,
+            message: "User Not Found",
+        });
+        return;
+    }
+
+    try {
+        const comp = await bcrypt.compareSync(user.password, check.password);
+        if (comp) {
+            const token = jwt.sign(
+                {
+                    email: user.email,
+                    firstName: check.firstName,
+                    lastName: check.lastName,
+                },
+                secret,
+                {
+                    expiresIn: 3600,
+                }
+            );
+            res.setHeader("Set-Cookie", `token=${token}; Max-Age=3600; Path=/`);
+            res.status(200).json({ success: true, message: "Logged in" });
+        } else {
+            res.status(401).json({
+                success: false,
+                message: "User Not Found",
+            });
+            return;
+        }
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Server Error, try again later",
+        });
+        return;
+    }
 };
 
 export default handler;
